d60350ad9acd0404a825975b939f01a8c3c2677a
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-01-03 11:29:40 +0530

    create pages and routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,3 +1,19 @@
-export default function App(){
-  return (<h1 className="text-red-500">Hello Jeffrey</h1>)
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/sign-up', element: <SignUp /> },
+  { path: '/sign-in', element: <SignIn /> },
+  { path: '/about', element: <About /> },
+  { path: '/profile', element: <Profile /> },
+
+])
+
+export default function App() {
+  return (<RouterProvider router={router} />)
 }
