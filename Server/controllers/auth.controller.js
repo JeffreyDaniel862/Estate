377e955508275fd9a8cf25471615f01beb9d267f
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-01-04 11:00:27 +0530

    complete signin route using bcrypt and jwt

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/errorHandler.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config()
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -8,6 +12,22 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.status(201).json({ message: "User saved successfully" });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(404, "Invalid email or password"));
+        const userObject = {email: validUser.email, username: validUser.username};
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_MAGIC);
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(userObject);
     } catch (error) {
         next(error);
     }
